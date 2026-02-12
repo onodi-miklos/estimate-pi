fbded56ffb4e06be74fe3551702ee36b5b558743
Miki <mikonodi33@gmail.com>
2026-02-12 16:31:06 +0100

    Continued app.ts

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,17 +1,17 @@
 // ESTIMATE PI
 
 const {addArea} = require('./area.js')
-
+const {results, generatePoints} = require('./points.js')
 
 let accuracy: number;
 accuracy = 0.1;
 
-interface result{
+interface Result{
   in: number,
   out: number
 }
 
-function calculatePi(args:result):number{}
+function calculatePi(args:Result):number{}
 function saveResultsToFile():void{}
 
 module.exports = { accuracy };
