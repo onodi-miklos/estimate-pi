373e51fe732ac5cf73f33d338f26b514b180a211
Miki <mikonodi33@gmail.com>
2026-02-12 16:23:20 +0100

    Continued app.ts
    Added interface 'result' and function stubs for calculating Pi and saving results.

--- app/src/app.ts
+++ app/src/app.ts
@@ -6,4 +6,12 @@ const {addArea} = require('./area.js')
 let accuracy: number;
 accuracy = 0.1;
 
+interface result{
+  in: number,
+  out: number
+}
+
+function calculatePi(args:result):number{}
+function saveResultsToFile():void{}
+
 module.exports = { accuracy };
