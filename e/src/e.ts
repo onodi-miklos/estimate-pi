f83602e4078b8a19e3eeb4e887c715b896e08556
Miki <mikonodi33@gmail.com>
2026-02-15 18:48:40 +0100

    Created e.ts

--- e/src/e.ts
+++ e/src/e.ts
@@ -1,1 +1,24 @@
+const fs = require('fs')
+const path = require('path')
 
+interface Result{
+    accuracy: number;
+    e: number
+}
+
+let results = Result[]
+
+function calculate(accuracy: number):number{
+    const newAccuracy = Math.pow(accuracy, -1)
+    
+    for (let i: number = 1; i <= accuracy; i++){
+        const result: Result = {
+            accuracy: i,
+            e: Math.pow((1 + (1/i)), i)
+        }
+        
+        results.push(result)
+    }
+}
+
+fs.writeFileSync(path.join(_dirname, ../result.txt), results)
